1ebd7cd00014bee2688421013d800be1983e5c5f
youssefmohamed93 <142684130+youssefmohamed93@users.noreply.github.com>
2023-12-22 02:15:44 +0200

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,72 @@
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('shadow', window.scrollY > 120 );
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView:2,
+        },
+        568: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        968: {
+            slidesPerView: 5,
+        },
+
+    }
+});
+
+const sr = ScrollReveal (
+  {
+      distance: '40px',
+      duration: 2050,
+      delay: 200,
+      reset: true
+  }
+);
+
+sr.reveal('.movies-container',{origin: 'top'});
+sr.reveal('.coming-container',{origin: 'top'});
+sr.reveal('.heading',{origin: 'top'});
+sr.reveal('.newsletter',{origin: 'top'});
+sr.reveal('.footer',{origin: 'top'});
+sr.reveal('p',{origin: 'top'});
